c8fc441c4533d51ec8098a18ec1552f1ac908335
ankush <ankush.1559@gmail.com>
2022-11-27 19:00:47 +0530

    Updating steps to 200

--- QCNN/Training.py
+++ QCNN/Training.py
@@ -33,7 +33,7 @@ def cost(params, X, Y, U, U_params, embedding_type, circuit, cost_fn):
     return loss
 
 # Circuit training parameters
-steps = 1
+steps = 200
 learning_rate = 0.01
 batch_size = 25
 def circuit_training(X_train, Y_train, U, U_params, embedding_type, circuit, cost_fn):
